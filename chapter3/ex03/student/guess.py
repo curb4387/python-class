ffee83d7020677c8ec983b37fde7e54fd7ae49e5
curb4387 <140212567+curb4387@users.noreply.github.com>
2024-09-26 23:48:02 -0500

    Update guess.py

--- chapter3/ex03/student/guess.py
+++ chapter3/ex03/student/guess.py
@@ -1,20 +1,35 @@
 # Write your program here
 # TKTK: Add started code here.
 import random
-from math import log
+import math
 
 smaller = int(input("Enter the smaller number: "))
 larger = int(input("Enter the larger number: "))
-myNumber = random.randint(smaller, larger)
-count = 0
 
-while True:
-    count += 1
-    userNumber = int(input("Enter your number: "))
-    if userNumber < myNumber:
-        print("Too small!")
-    elif userNumber > myNumber:
-        print("Too large!")
+max_guess = int(math.log(larger - smaller + 1, 2))
+count = 0
+guess = random.randint(smaller, larger)
+print("Your number is", guess)
+user_hint = input("Enter =, <, or >: ")
+
+while smaller <= larger or larger >= smaller:
+    while max_guess > count:
+        guess = random.randint(smaller, larger)
+        print("Your number is", guess)
+        user_hint = input("Enter =, <, or >: ")
+        count += 1
+        if user_hint == "<":
+            larger = guess - 1
+            print(smaller, larger)
+        elif user_hint == ">":
+            smaller = guess + 1
+            print(smaller, larger)
+        elif user_hint == "=":
+            print("Hooray, I've got it in", count, "tries!")
+            break
     else:
-        print("Congratulations! The computer guessed in", count, "tries!")
+        print("I'm out of guesses, and you cheated!")
         break
+    count += 1
+    break
+        
