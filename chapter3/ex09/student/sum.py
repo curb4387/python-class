c16f7e51b33a5a4164a6f2d68886c38dcad07305
curb4387 <140212567+curb4387@users.noreply.github.com>
2024-09-29 18:13:22 -0500

    Add files via upload

--- chapter3/ex09/student/sum.py
+++ chapter3/ex09/student/sum.py
@@ -1,1 +1,13 @@
 # Write your program here
+num_sum = 0.0
+counter = 0
+while True:
+    user_number = input("Enter a number or press Enter to quit: ")
+    if user_number == "":
+        break
+    number = float(user_number)
+    num_sum += number
+    counter += 1
+average = num_sum / counter
+print("The sum is", num_sum)
+print("The average is", average)
