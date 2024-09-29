38b6c807db87202af89153bfea21df81e0469763
curb4387 <140212567+curb4387@users.noreply.github.com>
2024-09-29 18:12:08 -0500

    Add files via upload

--- chapter3/ex08/student/gcd.py
+++ chapter3/ex08/student/gcd.py
@@ -1,1 +1,11 @@
 # Write your program here
+smaller = int(input("Enter the smaller number: "))
+larger = int(input("Enter the larger number: "))
+
+while smaller > 0:
+    remainder = larger % smaller
+    larger = smaller
+    smaller = remainder
+greatest_common_divisor = larger
+
+print("The greatest common divisor is", greatest_common_divisor)
