e60115e36f4423be453b523944937953f0bea192
curb4387 <140212567+curb4387@users.noreply.github.com>
2024-09-29 14:24:42 -0500

    Add files via upload

--- chapter3/ex04/student/bouncy.py
+++ chapter3/ex04/student/bouncy.py
@@ -1,1 +1,16 @@
 # Write your program here
+# This program calculates bounciness
+
+height_dropped = float(input("Enter height the ball is dropped at: "))
+bounce_index = float(input("Enter the bounciness index of the ball: "))
+bounce_number = int(input("Enter the number of times the ball is allowed to continue bouncing: "))
+
+total_distance = height_dropped
+current_height = height_dropped
+
+for bounce in range(bounce_number):
+    bounce_height = current_height * bounce_index
+    total_distance += 2 * bounce_height
+    current_height = bounce_height
+total_distance = total_distance - bounce_height
+print("Total distance traveled is:", total_distance, "units.")
