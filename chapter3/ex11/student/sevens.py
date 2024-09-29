30f3ffef35e69276331e796d835fbed82b9f8f58
curb4387 <140212567+curb4387@users.noreply.github.com>
2024-09-29 18:15:20 -0500

    Add files via upload

--- chapter3/ex11/student/sevens.py
+++ chapter3/ex11/student/sevens.py
@@ -1,1 +1,23 @@
 # Write your program here
+import random
+
+money_bet = int(input("How many dollars do you have?: "))
+initial_bet = money_bet
+max_money = initial_bet
+count = 0
+current_count = 0
+while money_bet > 0:
+    dice_1 = random.randint(1, 7)
+    dice_2 = random.randint(1, 7)
+    dice_sum = dice_1 + dice_2
+    count += 1
+    if dice_sum != 7:
+        money_bet -= 1
+    else:
+        money_bet += 4
+    if money_bet > initial_bet:
+        max_money = money_bet
+        current_count = count
+
+print("You are broke after", count, "rolls.")
+print("You should have quit after", current_count, "rolls when you had", "$%-2d" % max_money)
