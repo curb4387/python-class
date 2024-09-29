9ce0e18af2e8be379f0df9b93f2397a2165f6bba
curb4387 <140212567+curb4387@users.noreply.github.com>
2024-09-29 14:26:32 -0500

    Add files via upload

--- chapter3/ex05/student/population.py
+++ chapter3/ex05/student/population.py
@@ -1,1 +1,17 @@
 # Write your program here
+
+initial_organisms = float(input("Enter the initial number of organisms: "))
+growth_rate = float(input("Enter the rate of growth [a real number > 1]: "))
+hours_to_growth = float(input("Enter the number of hours to achieve the rate of growth: "))
+total_hours = float(input("Enter the total hours of growth: "))
+
+growth_time = int(total_hours / hours_to_growth)
+total_population = 0
+time = 0
+
+for time in range(growth_time):
+    total_population = initial_organisms * growth_rate
+    initial_organisms = total_population
+    time += 1
+
+print("The total population is", int(total_population))
