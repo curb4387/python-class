ea9df04d5192b4ce4acdb9e71350a6c504e599c5
curb4387 <140212567+curb4387@users.noreply.github.com>
2024-09-29 18:31:16 -0500

    Add files via upload

--- chapter3/dex01/student/computeAverage.py
+++ chapter3/dex01/student/computeAverage.py
@@ -1,8 +1,9 @@
 count = 1
 total = 0
-while count < 10:
+
+while count <= 10:
     score = int(input("Enter test score number " + str(count) + ": "))
     total = total + score
     count = count + 1
-average = total / count
+average = total / (count - 1)
 print("The average test score is", average)
