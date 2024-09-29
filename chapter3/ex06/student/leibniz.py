18682cb2bf1ea007c23d41c4a638073da8a94a23
curb4387 <140212567+curb4387@users.noreply.github.com>
2024-09-29 14:29:33 -0500

    Add files via upload

--- chapter3/ex06/student/leibniz.py
+++ chapter3/ex06/student/leibniz.py
@@ -1,1 +1,10 @@
 # Write your program here
+def leibniz_pi(iterations):
+    count = 0
+    for number in range(iterations):
+        leibniz = (-1) ** number / (2 * number + 1)
+        count += leibniz
+    return (count * 4)
+iterations = int(input("Enter the number of iterations: "))
+pi_approximation = leibniz_pi(iterations)
+print("The approximation of pi is", pi_approximation)
