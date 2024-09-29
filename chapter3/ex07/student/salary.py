a94551ab1c215b09b32b1855aca95bc66374e761
curb4387 <140212567+curb4387@users.noreply.github.com>
2024-09-29 14:31:17 -0500

    Add files via upload

--- chapter3/ex07/student/salary.py
+++ chapter3/ex07/student/salary.py
@@ -1,1 +1,12 @@
 # Write your program here
+salary = float(input("Enter the starting salary: "))
+percent_increase = float(input("Enter the annual % increase: "))
+years = int(input("Enter the number of years: " ))
+percent_increase = percent_increase / 100
+
+print("")
+print("Year", "%8s" % "Salary")
+print("-------------")
+for one_year in range(1, years + 1):
+    print("%2s" % one_year, "%12s" % f"${salary:.2f}")
+    salary += salary * percent_increase
